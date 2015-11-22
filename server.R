2b187f90d60e1e15c5de2a7b6708fae8b49c55bf
Carlos Cunha <ceacunha@Carloss-MacBook-Pro.local>
2015-11-22 15:22:30 -0200

    First Iteraction
    First Iteraction

--- server.R
+++ server.R
@@ -1,8 +1,39 @@
 library(shiny)
+library(ggplot2)
+library(reshape2)
+
+population <- read.csv("populational.data.csv",header = FALSE, sep = ";", stringsAsFactors = FALSE)
+colnames(population) <-
+    c("State",
+      "2000","2001","2002","2003","2004","2005","2006","2007","2008","2009",
+      "2010","2011","2012","2013","2014","2015","2016","2017","2018","2019",
+      "2020","2021","2022","2023","2024","2025","2026","2027","2028","2029",
+      "2030")
+population <- melt(population, "State")
+colnames(population) <- c("State", "Year", "Population")
+population$Population <-
+    round(x = population$Population/1000000, digits = 2)
+population$Year <- as.numeric(as.character(population$Year))
+
 shinyServer(
-    function(input, output){   
-        output$outId1 <- renderPrint({input$id1})
-        output$outId2 <- renderPrint({input$id2})
-        output$outId3 <- renderPrint({input$id3})
+    function(input, output){
+        
+        output$plotOutput <- renderPlot({
+            
+            newPopulation <-
+                subset(population,
+                       Year >= input$inMinYear & Year <= input$inMaxYear)
+            
+            newPopulation <-
+                subset(newPopulation, State %in% input$inState)
+            
+            newPopulation <-
+                subset(newPopulation, Population >= input$inMinStatePopulation)
+            
+            ggplot(newPopulation, aes(x = Year, y = Population)) +
+                geom_point(aes(colour = State)) + ylab("Population in Millions") +
+                xlab("Year")
+        })
+        
     }
 )
--- ui.R
+++ ui.R
@@ -1,26 +1,59 @@
 library(shiny)
 shinyUI(
-    pageWithSidebar(
-        headerPanel("Data Science FTW!"),
-        sidebarPanel(
-            numericInput(
-                inputId = "id1", label = "Numeric Input",
-                value = 0, min = 1, max = 10),
-            checkboxGroupInput(
-                inputId = "id2",label = "checkbox",
-                choices = c("Value 1" = "1", "Value 2" = "2")
+    fluidPage(
+        headerPanel("Brazilian Population Growth Study"),
+        sidebarLayout(
+            fluid = TRUE,
+            sidebarPanel(
+                width = 5,
+                tags$label("Search Period: ", class="centered", width="200px"),
+                tags$label("From", width = "20px"),
+                tags$input(id = "inMinYear", style = "width:70px",
+                           value = 2010, min = 2000, max = 2030, type="number"),
+                tags$label("To"),
+                tags$input(id = "inMaxYear", style = "width:70px", value = 2015, min = 2000, max = 2030, type="number"),
+                
+                numericInput(
+                    inputId = "inMinStatePopulation", label = "Minimun State Population (in Milliions)",
+                    value = 1, min = 0.1, max = 200),
+                checkboxGroupInput(
+                    inputId = "inState",label = "States",inline = TRUE,
+                    choices = 
+                        c(
+                    "Acre - AC" = "AC",
+                    "Alagoas - AL" = "AL",
+                    "Amapá - AP" = "AP",
+                    "Amazonas - AM" = "AM",
+                    "Bahia - BA" = "BA",
+                    "Ceará - CE" = "CE",
+                    "Distrito Federal - DF" = "DF",
+                    "Espírito Santo - ES" = "ES",
+                    "Goiás - GO" = "GO",
+                    "Maranhão - MA" = "MA",
+                    "Mato Grosso - MT" = "MT",
+                    "Mato Grosso do Sul - MS" = "MS",
+                    "Minas Gerais - MG" = "MG",
+                    "Pará - PA" = "PA",
+                    "Paraíba - PB" = "PB",
+                    "Paraná - PR" = "PR",
+                    "Pernambuco - PE" = "PE",
+                    "Piauí - PI" = "PI",
+                    "Rio de Janeiro - RJ" = "RJ",
+                    "Rio Grande do Norte - RN" = "RN",
+                    "Rio Grande do Sul - RS" = "RS",
+                    "Rondônia - RO" = "RO",
+                    "Roraima - RR" = "RR",
+                    "Santa Catarina - SC" = "SC",
+                    "São Paulo - SP" = "SP",
+                    "Sergipe - SE" = "SE",
+                    "Tocantins - TO" = "TO"),
+                    selected = c("SP", "RJ", "DF", "PE", "RS")
+                )
             ),
-            dateInput(
-                inputId = "id3", label = "Date"
+            mainPanel(
+                width = 7,
+                plotOutput(outputId = "plotOutput")
             )
-        ),
-        mainPanel(
-            h4("Numeric Input"),
-            verbatimTextOutput("outId1"),
-            h4("checked"),
-            verbatimTextOutput("outId2"),
-            h4("Ðate"),
-            verbatimTextOutput("outId3")
         )
     )
 )
